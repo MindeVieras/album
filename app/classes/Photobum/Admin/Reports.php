ccc9cc23c21565cb53154055aefe351f9f5d3905
mindevieras <wieraz21@gmail.com>
2017-02-12 23:09:08 +0000

    reposrt dir status check done

--- app/classes/Photobum/Admin/Reports.php
+++ app/classes/Photobum/Admin/Reports.php
@@ -18,9 +18,40 @@ class Reports extends Admin{
         $this->auth();
         $template = $this->twig->loadTemplate('Admin/reports.html');
         echo $template->render([
+        	'dir_status' => $this->getDirStatus(),
             'page' => $this->page,
             'user' => $this->user
         ]);
     }
 
+    public function getDirStatus(){
+   		$ds = DIRECTORY_SEPARATOR;
+        $dirs = array('uploads', 'media', 'media'.$ds.'albums', 'media'.$ds.'styles');
+        
+        // check directories
+        foreach ($dirs as $d) {
+            
+            $upl_dir = array(
+                'name' => $d,
+                'ack' => 'ok',
+                'status' => 'All is good.'
+            );
+            if(!is_dir(getcwd().$ds.$d)){
+                $upl_dir = array(
+                    'name' => $d,
+                    'ack' => 'error',
+                    'status' => 'Doesn\'t exist.'
+                );
+            } elseif (!is_writable(getcwd().$ds.$d)){
+                $upl_dir = array(
+                    'name' => $d,
+                    'ack' => 'error',
+                    'status' => 'Not writable.'
+                );
+            }
+            $data[] = $upl_dir;
+        }
+
+    	return $data;
+    }
 }
