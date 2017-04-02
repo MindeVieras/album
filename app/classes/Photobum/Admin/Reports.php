c7c97f1ce437362ac1f5a0cc4336c8da9ae229a2
mindevieras <wieraz21@gmail.com>
2017-04-02 22:24:53 +0100

    reports style

--- app/classes/Photobum/Admin/Reports.php
+++ app/classes/Photobum/Admin/Reports.php
@@ -12,6 +12,7 @@ class Reports extends Admin{
     public function __construct(){
         parent::__construct();
         $this->page['title']= 'Reports';
+        $this->page['body_class']= 'reports';
         $this->page['section']= 'reports';
     }
 
@@ -27,27 +28,27 @@ class Reports extends Admin{
 
     public function getDirStatus(){
    		$ds = DIRECTORY_SEPARATOR;
-        $dirs = array('uploads', 'media', 'media'.$ds.'albums', 'media'.$ds.'styles');
+        $dirs = array('uploads', 'media', 'media'.$ds.'albums', 'media'.$ds.'persons', 'media'.$ds.'users');
         
         // check directories
         foreach ($dirs as $d) {
             
             $upl_dir = array(
-                'name' => $d,
+                'path' => $d,
                 'ack' => 'ok',
-                'status' => 'All is good.'
+                'msg' => 'Directory is healthy.'
             );
             if(!is_dir(getcwd().$ds.$d)){
                 $upl_dir = array(
-                    'name' => $d,
+                    'path' => $d,
                     'ack' => 'error',
-                    'status' => 'Doesn\'t exist.'
+                    'msg' => 'Directory doesn\'t exist.'
                 );
             } elseif (!is_writable(getcwd().$ds.$d)){
                 $upl_dir = array(
-                    'name' => $d,
-                    'ack' => 'error',
-                    'status' => 'Not writable.'
+                    'path' => $d,
+                    'ack' => 'warn',
+                    'msg' => 'Dirctory not writable.'
                 );
             }
             $data[] = $upl_dir;
